93d7e46875ba5fb877c2c3ad29f6ac57ef06bad6
azazi-sa <sameerazazi87@gmail.com>
2017-07-08 12:45:08 +0530

    add hw app

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace src.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "hello world " + DateTime.Now.ToString();
+        }
+    }
+}
